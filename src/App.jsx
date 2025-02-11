2e6b1ccda2cba64a53afb8c60c33a291a89a3b4e
gadisa kasaye <gadisaka04@gmail.com>
2025-02-11 09:44:12 +0300

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-green-500 w-full h-screen">
+      <h1 className="text-6xl font-bold text-white">Fira </h1>
+    </div>
+  );
+}
+
+export default App;
